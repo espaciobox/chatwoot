63648a96b7bf67f13be846d9e4f3171fb8ca2052
espaciobox <contacto.espaciobox@gmail.com>
2025-04-19 21:44:47 -0400

    Add files via upload

---
+++ .yarn/releases/yarn-3.2.0.cjs
@@ -0,0 +1,5 @@
+
+// THIS IS A PLACEHOLDER for yarn-3.2.0.cjs
+// In production, this file should be generated using: `yarn set version 3.2.0`
+// DO NOT use this content in production environments. Only for deployment structure validation.
+console.log("Simulated yarn-3.2.0.cjs loaded");
